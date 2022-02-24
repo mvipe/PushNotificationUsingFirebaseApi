2c695e9aef80824132edecd26229ae414b0dd4f6
The Vipe <mohnishvats@gmail.com>
2022-02-23 20:44:40 -0800

    Update constants.java
    updating constants

--- app/src/main/java/com/learn/pushnotificationusingapi/constants.java
+++ app/src/main/java/com/learn/pushnotificationusingapi/constants.java
@@ -2,7 +2,7 @@ package com.learn.pushnotificationusingapi;
 
 public class constants {
     public static final String BASE_URL="https://fcm.googleapis.com";
-    public static final String SERVER_KEY="AAAANbXAI5s:APA91bE573vvKyScp4N0Sa7JmmZM-RYaoq0cPt6WsZpe8L8zb3xHSEieoRl_r0KHovlmGNKvbBUaROaHtaZOGxKGi9Ks-PmtOq1ZxtFt3zumkFnFiH37048zPfW3URN7f1np1KQMmofj";
+    public static final String SERVER_KEY="";
     public static final String CONTENT_TYPE="application/json";
     public static final String TOPIC="/topics/truthy";
 
